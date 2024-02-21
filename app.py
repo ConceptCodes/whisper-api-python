699640f5e5b174190b26021ff60915d3cd1dd0d9
concept <conceptcodes@gmail.com>
2024-02-20 23:46:41 -0500

    Add FastAPI and v1 API router

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import uvicorn
-from v1.router import api
+from fastapi import FastAPI 
+from v1.api import api as v1_api
 from lib.log import logger
 
 if __name__ == "__main__":
     logger.info("Starting server... ")
+
+    api = FastAPI()
+    api.include_router(v1_api, prefix="/v1")
+
     uvicorn.run(api, host="0.0.0.0", port=80)
