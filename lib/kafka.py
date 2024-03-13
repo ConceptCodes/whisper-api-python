f1aad5559e4f8664984f1fec0529a9d098a3af12
concept <conceptcodes@gmail.com>
2024-03-13 19:15:28 -0400

    Refactor Kafka class to remove consumer

--- lib/kafka.py
+++ lib/kafka.py
@@ -1,4 +1,4 @@
-from confluent_kafka import Producer, Consumer, KafkaException
+from confluent_kafka import Producer
 from core.config import app_config
 from lib.logger import logger
 
@@ -14,32 +14,11 @@ class Kafka:
     def __init__(self):
         self.producer = Producer(
             {'bootstrap.servers': app_config.KAFKA_BOOTSTRAP_SERVERS})
-        self.consumer = Consumer({
-            'bootstrap.servers': app_config.KAFKA_BOOTSTRAP_SERVERS,
-            'group.id': app_config.KAFKA_GROUP_ID,
-            'auto.offset.reset': 'earliest'
-        })
-        self.consumer.subscribe([app_config.KAFKA_TOPIC])
 
     def send_message(self, topic, message):
         logger.info(f"Sending message to {topic}: {message}")
         self.producer.produce(topic, message)
         self.producer.flush()
-
-    def receive_message(self):
-        try:
-            while True:
-                msg = self.consumer.poll(1.0)
-                if msg is None:
-                    continue
-                if msg.error():
-                    raise KafkaException(msg.error())
-                else:
-                    return msg.value().decode('utf-8')
-        except KeyboardInterrupt:
-            pass
-        finally:
-            self.consumer.close()
 
 
 kafka_client = Kafka()
